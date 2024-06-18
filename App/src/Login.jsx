a9440e82e3780bea4c6b8781f815bf5e2df83e13
Hemdat <hemdatse@gmail.com>
2024-06-18 19:53:21 +0300

    adding style to the login page

--- App/src/Login.jsx
+++ App/src/Login.jsx
@@ -7,56 +7,56 @@ export default function Login() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Form />}>
-          <Route path='App' element={<App />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Form />}>
+            <Route path='App' element={<App />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
 function Form() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      navigate('/App');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    navigate('/App');
     // try {
     //   await authenticate(username, password); // Assuming you have the authenticate function
     //   navigate('ome');
-       
- //   } catch {
-  //      setError('Invalid username or password');
-  //    }
-    };
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          {error && <p>{error}</p>}
-          <input type="submit" value="Submit" />
-        </form>
-    
+
+    //   } catch {
+    //      setError('Invalid username or password');
+    //    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {error && <p>{error}</p>}
+        <input type="submit" value="Submit" />
+      </form>
+
     </>
-    )
-  }
+  )
+}
