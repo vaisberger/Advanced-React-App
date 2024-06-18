e1893e51dd45323e3d56dafc4e52f9b6e55ef78c
vaisberger <yaelw123@gmail.com>
2024-06-18 18:04:00 +0300

    18.6 2

---
+++ App/src/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+   try {
+     await authenticate(username, password); // Assuming you have the authenticate function
+     
+     
+  } catch {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      {error && <p>{error}</p>}
+      <input type="submit" value="Submit" />
+    </form>
+
+</>
+  );
+}
