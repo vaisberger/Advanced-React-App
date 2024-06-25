1c6d918a1fe614dfc3977a76b564f0b3e3faffb7
Hemdat <hemdatse@gmail.com>
2024-06-25 09:21:10 +0300

    update design

--- App/src/Home.jsx
+++ App/src/Home.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './Layout'
-import Posts from './Posts';
-import Todos from './Todos';
-import Albums from './Albums';
+import React, { useState } from 'react';
+import Layout from './Layout';
 import classes from './App.module.css';
+
 const Home = () => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+
   return (
-   <Layout/>
+    <Layout>
+      <div className={classes['home-container']}>
+        <h1 className={classes['home-title']}>Welcome to the Home Page!</h1>
+        <h2 className={classes['home-welcome']}>Hello, {user.username}!</h2>
+        <p className={classes['home-content']}>
+          This is your personalized home page. Here you can navigate to different sections of the application and manage your content.
+        </p>
+      </div>
+    </Layout>
   );
 };
 
