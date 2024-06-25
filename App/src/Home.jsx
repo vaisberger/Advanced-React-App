3464a9688c0482b5cf4973e412c67ee81dad0ecf
vaisberger <yaelw123@gmail.com>
2024-06-25 09:27:36 +0300

    hom page start

--- App/src/Home.jsx
+++ App/src/Home.jsx
@@ -6,15 +6,17 @@ const Home = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
 
   return (
-    <Layout>
-      <div className={classes['home-container']}>
+    <> <Layout/>
+          <div className={classes['home-container']}>
         <h1 className={classes['home-title']}>Welcome to the Home Page!</h1>
         <h2 className={classes['home-welcome']}>Hello, {user.username}!</h2>
         <p className={classes['home-content']}>
           This is your personalized home page. Here you can navigate to different sections of the application and manage your content.
         </p>
       </div>
-    </Layout>
+    </>
+   
+
   );
 };
 
